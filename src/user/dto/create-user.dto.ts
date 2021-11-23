f045c54cf868df694fba218210f3d717f2c88218
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-24 00:00:50 +0300

    Feat - Define the create user dto for validation of requests

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  firstName: string;
+
+  @IsNotEmpty()
+  lastName: string;
+
+  @IsNotEmpty()
+  username: string;
+
+  @IsNotEmpty()
+  password: string;
+}
