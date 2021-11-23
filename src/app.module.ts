92f8b93968f366f213d9d6f8071f0854877ee77c
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-23 23:59:51 +0300

    Chore - Register the user module in the main app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     TypeOrmModule.forRoot(),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
