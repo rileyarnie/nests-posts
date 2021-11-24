57d7da8b37c8a68dcddd67fb4ff5201cebce8914
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-25 02:48:06 +0300

    Feat - Define entity for a single posts and add a many to one relationship with user

--- src/posts/entities/post.entity.ts
+++ src/posts/entities/post.entity.ts
@@ -1,1 +1,23 @@
-export class Post {}
+import { User } from 'src/user/entities/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('posts')
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  body: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  authorId: number;
+}
