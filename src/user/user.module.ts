e993b97570698361c4608283d007c5ad165920a2
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-24 21:30:26 +0300

    Feat - Use dependency injection to inject the config module to the user module to allow use on environment variables

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,24 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '3600s',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService, AuthService],
 })
