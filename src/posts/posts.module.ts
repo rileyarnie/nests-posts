23855dfbcba2c0d10c7acd5148825738e4c37852
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-25 02:49:34 +0300

    Chore - Register all required dependencies in teh posts module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from './entities/post.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post]), UserModule],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService],
 })
 export class PostsModule {}
