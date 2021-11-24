bd3776f6070f762aa4771e2079d34a93eb3f0ef3
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-24 15:02:38 +0300

    Chore - Add authservice to the list of providers in the user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, AuthService],
 })
 export class UserModule {}
