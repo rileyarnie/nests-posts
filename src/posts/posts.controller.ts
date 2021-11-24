954b4c735ac1ecb3e1cd00c8c25f342376b05583
Arnold Kibira <arnold.ril3y@gmail.com>
2021-11-25 02:48:55 +0300

    Feat - Implement the create post controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { AuthGuard } from 'src/user/guards/auth.guard';
+import { GetUser } from 'src/user/decorators/getuser.decorator';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postsService.create(createPostDto);
+  @UseGuards(AuthGuard)
+  @Post('new')
+  create(
+    @Body() createPostDto: CreatePostDto,
+    @GetUser('access_token') access_token: string,
+  ) {
+    return this.postsService.create(createPostDto, access_token);
   }
 
   @Get()
